9b4341cbd49fb3e38e64733554549278a3ca876d
Sam LeDoux <sledoux@google.com>
2023-09-14 11:05:09 -0400

    make the path simpler

--- tests/fps_tests.py
+++ tests/fps_tests.py
@@ -5,7 +5,7 @@ from publicsuffix2 import PublicSuffixList
 from unittest import mock
 from requests import structures
 
-sys.path.append('tests/..')
+sys.path.append('.')
 from FpsSet import FpsSet
 from FpsCheck import FpsCheck
 from check_sites import find_diff_sets
